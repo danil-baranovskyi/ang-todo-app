5079f91f6a34b2b766770dac7bde37a7f0960744
danil.baranovskyi <danbaranovskyi@gmail.com>
2024-07-01 09:39:30 +0300

    Initialized app, added main card, + header with search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainBoardComponent} from "./core/layout/main-board/main-board.component";
+import {IconService} from "./shared/services/icon.services";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainBoardComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ang-todo-app';
+export class AppComponent implements OnInit {
+  private iconService = inject(IconService);
+
+  ngOnInit() {
+    this.iconService.init();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {TaskComponent} from "./features/task/task.component";
+import {MainBoardComponent} from "./core/layout/main-board/main-board.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainBoardComponent,
+    children: [
+      {
+        path: '',
+        component: TaskComponent,
+      }
+    ]
+  },
+];
